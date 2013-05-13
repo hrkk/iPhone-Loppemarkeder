dfa34beae22bf4370c6d8ab218ff6024a8e2bde6
Thomas Sandvik <thsandvik@gmail.com>
2013-05-13 12:23:40 +0200

    Initial

---
+++ NetworkingIsFun/ViewController.h
@@ -0,0 +1,13 @@
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController <UITableViewDataSource, UITableViewDelegate> {
+    UITableView *_tableView;
+    UIActivityIndicatorView *_activityIndicatorView;
+    NSArray *_marketItemsArray;
+}
+
+@property (nonatomic, retain) UITableView *tableView;
+@property (nonatomic, retain) UIActivityIndicatorView *activityIndicatorView;
+@property (nonatomic, retain) NSArray *marketItemsArray;
+
+@end
---
+++ NetworkingIsFun/ViewController.m
@@ -0,0 +1,79 @@
+#import "ViewController.h"
+
+#import "AFNetworking.h"
+
+@implementation ViewController
+
+@synthesize tableView = _tableView, activityIndicatorView = _activityIndicatorView, marketItemsArray = _marketItemsArray;
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    // Setting Up Table View
+    self.tableView = [[UITableView alloc] initWithFrame:CGRectMake(0.0, 0.0, self.view.bounds.size.width, self.view.bounds.size.height) style:UITableViewStylePlain];
+    self.tableView.dataSource = self;
+    self.tableView.delegate = self;
+    self.tableView.autoresizingMask = UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleHeight;
+    self.tableView.hidden = YES;
+    [self.view addSubview:self.tableView];
+    
+    // Setting Up Activity Indicator View
+    self.activityIndicatorView = [[UIActivityIndicatorView alloc] initWithActivityIndicatorStyle:UIActivityIndicatorViewStyleGray];
+    self.activityIndicatorView.hidesWhenStopped = YES;
+    self.activityIndicatorView.center = self.view.center;
+    [self.view addSubview:self.activityIndicatorView];
+    [self.activityIndicatorView startAnimating];
+    
+    // Initializing Data Source
+    self.marketItemsArray = [[NSArray alloc] init];
+    
+    NSURL *url = [[NSURL alloc] initWithString:@"http://roninit.dk/LoppemarkederAdminApp/markedItem/listJSON"];
+    NSURLRequest *request = [[NSURLRequest alloc] initWithURL:url];
+    
+    AFJSONRequestOperation *operation = [AFJSONRequestOperation JSONRequestOperationWithRequest:request success:^(NSURLRequest *request, NSHTTPURLResponse *response, id JSON) {
+		self.marketItemsArray = [JSON objectForKey:@"markedItemInstanceList"];
+        [self.activityIndicatorView stopAnimating];
+        [self.tableView setHidden:NO];
+        [self.tableView reloadData];
+        
+    } failure:^(NSURLRequest *request, NSHTTPURLResponse *response, NSError *error, id JSON) {
+        NSLog(@"Request Failed with Error: %@, %@", error, error.userInfo);
+    }];
+    
+    [operation start];
+}
+
+- (void)viewDidUnload {
+    [super viewDidUnload];
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation {
+    return YES;
+}
+
+// Table View Data Source Methods
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    if (self.marketItemsArray && self.marketItemsArray.count) {
+        return self.marketItemsArray.count;
+    } else {
+        return 0;
+    }
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    static NSString *cellID = @"Cell Identifier";
+    
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:cellID];
+    
+    if (!cell) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleSubtitle reuseIdentifier:cellID];
+    }
+    
+    NSDictionary *movie = [self.marketItemsArray objectAtIndex:indexPath.row];
+    cell.textLabel.text = [movie objectForKey:@"address"];
+    cell.detailTextLabel.text = [movie objectForKey:@"dateExtraInfo"];
+    
+    return cell;
+}
+
+@end
