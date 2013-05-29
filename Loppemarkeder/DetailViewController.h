f109799dc11ef8ac13c48df85dcb8be5adbc01a0
Thomas H. Sandvik <thomashsandvik@Thomass-MacBook-Pro.local>
2013-05-29 08:36:10 +0200

    Automatisk label højder go positioning i Detailbilled

--- Loppemarkeder/DetailViewController.h
+++ Loppemarkeder/DetailViewController.h
@@ -1,19 +1,25 @@
 #import <UIKit/UIKit.h>
 #import "MarketPlace.h"
 
-@interface DetailViewController : UIViewController<MKMapViewDelegate>
+@interface DetailViewController : UIViewController<MKMapViewDelegate,UIScrollViewDelegate>
 {
 	CLLocationManager *locationManager;
 	CLLocation *location;
 	float latitude, longitude;
 }
-
+@property (nonatomic, retain) IBOutlet UIScrollView *itemsContainerScroll;
 @property (nonatomic, strong) MarketPlace *marketplace;
 @property (strong, nonatomic) IBOutlet UILabel *addressLabel;
+@property (strong, nonatomic) IBOutlet UILabel *dateHeadline;
 @property (strong, nonatomic) IBOutlet UILabel *dateLabel;
+@property (strong, nonatomic) IBOutlet UILabel *entranceHeadline;
 @property (strong, nonatomic) IBOutlet UILabel *entranceLabel;
+@property (strong, nonatomic) IBOutlet UILabel *rulesHeadline;
 @property (strong, nonatomic) IBOutlet UILabel *rulesLabel;
 
+@property (strong, nonatomic) IBOutlet UILabel *marketHeadline;
 @property (strong, nonatomic) IBOutlet UILabel *marketInfo;
+@property (strong, nonatomic) IBOutlet UILabel *bookingHeadline;
+@property (strong, nonatomic) IBOutlet UILabel *bookingInfo;
 - (IBAction)showRoute:(id)sender;
 @end
--- Loppemarkeder/DetailViewController.m
+++ Loppemarkeder/DetailViewController.m
@@ -26,6 +26,98 @@
 	
 	// Create an instance of CLLocation
 	location=[locationManager location];
+    
+    [self adjustLabelsPosition];
+}
+
+-(void)adjustLabelsPosition
+{
+    //Calculate the expected size based on the font and linebreak mode of your label
+    // FLT_MAX here simply means no constraint in height
+    CGSize maximumLabelSize = CGSizeMake(296, FLT_MAX);
+    CGSize expectedLabelSize;
+    CGRect newFrame;
+    
+    //AdressInfo
+    maximumLabelSize = CGSizeMake(185, FLT_MAX);
+    expectedLabelSize = [_addressLabel.text sizeWithFont:_addressLabel.font constrainedToSize:maximumLabelSize lineBreakMode:_addressLabel.lineBreakMode];
+    newFrame = _addressLabel.frame;
+    newFrame.size.height = expectedLabelSize.height;
+    _addressLabel.frame = newFrame;
+    
+    
+     maximumLabelSize = CGSizeMake(296, FLT_MAX);
+    
+    //Datoheadling
+    newFrame = _dateHeadline.frame;
+    newFrame.origin.x = 20;
+    newFrame.origin.y = _addressLabel.frame.origin.y +   _addressLabel.frame.size.height + 5;
+    _dateHeadline.frame = newFrame;
+
+    //Datolabel
+    expectedLabelSize = [_dateLabel.text sizeWithFont:_dateLabel.font constrainedToSize:maximumLabelSize lineBreakMode:_dateLabel.lineBreakMode];
+    newFrame = _dateLabel.frame;
+    newFrame.size.height = expectedLabelSize.height;
+    newFrame.origin.x = 20;
+    newFrame.origin.y = _dateHeadline.frame.origin.y +   _dateHeadline.frame.size.height;
+    _dateLabel.frame = newFrame;
+    
+    //EntreHeadline
+    newFrame = _entranceHeadline.frame;
+    newFrame.origin.x = 20;
+    newFrame.origin.y = _dateLabel.frame.origin.y +   _dateLabel.frame.size.height + 5;
+    _entranceHeadline.frame = newFrame;
+    
+    //Entrelabel
+    expectedLabelSize = [_entranceLabel.text sizeWithFont:_entranceLabel.font constrainedToSize:maximumLabelSize lineBreakMode:_entranceLabel.lineBreakMode];
+    newFrame = _entranceLabel.frame;
+    newFrame.size.height = expectedLabelSize.height;
+    newFrame.origin.x = 20;
+    newFrame.origin.y = _entranceHeadline.frame.origin.y +   _entranceHeadline.frame.size.height;
+    _entranceLabel.frame = newFrame;
+
+    //ReglerHeadling
+    newFrame = _rulesHeadline.frame;
+    newFrame.origin.x = 20;
+    newFrame.origin.y = _entranceLabel.frame.origin.y +   _entranceLabel.frame.size.height + 5;
+    _rulesHeadline.frame = newFrame;
+    
+    //ReglerLabel
+    expectedLabelSize = [_rulesLabel.text sizeWithFont:_rulesLabel.font constrainedToSize:maximumLabelSize lineBreakMode:_rulesLabel.lineBreakMode];
+    newFrame = _rulesLabel.frame;
+    newFrame.size.height = expectedLabelSize.height;
+    newFrame.origin.x = 20;
+    newFrame.origin.y = _rulesHeadline.frame.origin.y +   _rulesHeadline.frame.size.height;
+    _rulesLabel.frame = newFrame;
+    
+    NSLog(@"%@",_rulesLabel.text);
+  
+    //MarketHeadling
+    newFrame = _marketHeadline.frame;
+    newFrame.origin.x = 20;
+    newFrame.origin.y = _rulesLabel.frame.origin.y +   _rulesLabel.frame.size.height + 5;
+    _marketHeadline.frame = newFrame;
+    
+   
+    //Markedsinfomation
+    expectedLabelSize = [_marketInfo.text sizeWithFont:_marketInfo.font constrainedToSize:maximumLabelSize lineBreakMode:_marketInfo.lineBreakMode];
+    newFrame = _marketInfo.frame;
+    newFrame.size.height = expectedLabelSize.height;
+    newFrame.origin.x = 20;
+    newFrame.origin.y = _marketHeadline.frame.origin.y +   _marketHeadline.frame.size.height;
+    _marketInfo.frame = newFrame;
+        
+    //Bookingheadling
+    newFrame = _bookingHeadline.frame;
+    newFrame.origin.x = 20;
+    newFrame.origin.y = _marketInfo.frame.origin.y +   _marketInfo.frame.size.height +10;
+    _bookingHeadline.frame = newFrame;
+
+    //BookingINfo
+    newFrame = _bookingInfo.frame;
+    newFrame.origin.x = 20;
+    newFrame.origin.y = _bookingHeadline.frame.origin.y +   _bookingHeadline.frame.size.height;
+    _bookingInfo.frame = newFrame;
 }
 
 #if __IPHONE_OS_VERSION_MIN_REQUIRED < __IPHONE_6_0
