09f509262439ae74912e051fab57a3932303626a
kasper odgaard <odgaard-kasper76@get2net.dk>
2013-06-06 21:50:35 +0200

    backbutton background and textcolor change

--- Loppemarkeder/MenuNavigationViewController.m
+++ Loppemarkeder/MenuNavigationViewController.m
@@ -15,6 +15,8 @@
 #import "Utilities.h"
 #import "MarketPlace.h"
 
+#define UIColorFromRGB(rgbValue) [UIColor colorWithRed:((float)((rgbValue & 0xFF0000) >> 16))/255.0 green:((float)((rgbValue & 0xFF00) >> 8))/255.0 blue:((float)(rgbValue & 0xFF))/255.0 alpha:1.0];
+
 
 @interface MenuNavigationViewController ()
 
@@ -31,10 +33,26 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+    
 	// Do any additional setup after loading the view.
 	// create a custom navigation bar button and set it to always says "Back"
 	UIBarButtonItem *temporaryBarButtonItem = [[UIBarButtonItem alloc] init];
-	temporaryBarButtonItem.title = @"Tilbage";
+	
+    temporaryBarButtonItem.title = @"Tilbage";
+    temporaryBarButtonItem.tintColor = [UIColor blackColor];
+    
+    NSString *customYellow = @"FFCD05";
+    int b =0;
+    sscanf([customYellow UTF8String],"%x",&b);
+    UIColor* btnColor = UIColorFromRGB(b);
+    
+    NSDictionary *attributes = [NSDictionary dictionaryWithObjectsAndKeys:
+                               btnColor,UITextAttributeTextColor,
+                                nil];
+    
+    [[UIBarButtonItem appearance] setTitleTextAttributes:attributes
+                                                forState:UIControlStateNormal];
+       
 	self.navigationItem.backBarButtonItem = temporaryBarButtonItem;
     
     // Setting Up Activity Indicator View
